3474509349a5d155ca50ffd3f262f68e0afc2863
Abdul Reyza <abdulreyza6@gmail.com>
2026-06-16 07:35:54 +0700

    bikin auth service

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  User? get currentUser => _auth.currentUser;
+
+  Stream<User?> get authStateChanges => _auth.authStateChanges();
+
+  Future<void> register({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    final credential = await _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+
+    await _firestore.collection('users').doc(credential.user!.uid).set({
+      'name': name,
+      'email': email,
+    });
+  }
+
+  Future<void> login({required String email, required String password}) async {
+    await _auth.signInWithEmailAndPassword(email: email, password: password);
+  }
+
+  Future<void> logout() async {
+    await _auth.signOut();
+  }
+}
