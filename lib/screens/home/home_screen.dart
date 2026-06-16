9c3a57f9241cfd8514d74cef666f817c0f640d58
Abdul Reyza <abdulreyza6@gmail.com>
2026-06-16 08:56:06 +0700

    bikin widgetbuild home

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -5,4 +5,39 @@ import '../../providers/auth_provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF5F7FA),
+
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: const Color(0xFFE3350D),
+        title: const Text(
+          'Pokemon Marketplace',
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              context.read<AuthProvider>().logout();
+            },
+            icon: const Icon(Icons.logout, color: Colors.white),
+          ),
+        ],
+      ),
+
+    
+      
+
+         
+
+        
+
+          
+        ),
+      ),
+    );
+  }
 }
