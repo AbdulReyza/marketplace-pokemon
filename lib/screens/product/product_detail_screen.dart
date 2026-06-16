52f85ce56db6c5acf4a6dc29635fb1d2c6f20689
Abdul Reyza <abdulreyza6@gmail.com>
2026-06-16 12:55:43 +0700

    bikin product detail

---
+++ lib/screens/product/product_detail_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../../models/product_model.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  final ProductModel product;
+
+  const ProductDetailScreen({super.key, required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    final currency = NumberFormat.currency(
+      locale: 'id_ID',
+      symbol: 'Rp ',
+      decimalDigits: 0,
+    );
+  }
+}
