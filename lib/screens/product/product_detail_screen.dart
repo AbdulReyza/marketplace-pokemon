9f5b9e3786dfa18f7506e220297ac2b3db2abb1c
Abdul Reyza <abdulreyza6@gmail.com>
2026-06-16 12:56:02 +0700

    bikin scaffold di detail

--- lib/screens/product/product_detail_screen.dart
+++ lib/screens/product/product_detail_screen.dart
@@ -15,5 +15,60 @@ class ProductDetailScreen extends StatelessWidget {
       symbol: 'Rp ',
       decimalDigits: 0,
     );
+
+    return Scaffold(
+      backgroundColor: const Color(0xFFF5F7FA),
+
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 350,
+            pinned: true,
+            backgroundColor: const Color(0xFFE3350D),
+
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(
+                product.name,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(color: Colors.white, fontSize: 16),
+              ),
+
+              background: Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Color(0xFFE3350D), Color(0xFF3B82F6)],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  ),
+                ),
+                child: Center(
+                  child: Hero(
+                    tag: product.id,
+                    child: Padding(
+                      padding: const EdgeInsets.all(40),
+                      child: Image.network(product.image, fit: BoxFit.contain),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.favorite_border, color: Colors.white),
+              ),
+            ],
+          ),
+
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
