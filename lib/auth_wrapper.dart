161beb461ee7de5ac0fb4523f806df1e69bc20dd
Abdul Reyza <abdulreyza6@gmail.com>
2026-06-16 09:19:27 +0700

    bikin auth wrapper

---
+++ lib/auth_wrapper.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'screens/auth/login_screen.dart';
+import 'screens/home/home_screen.dart';
+
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.data != null) {
+          return const HomeScreen();
+        }
+
+        return const LoginScreen();
+      },
+    );
+  }
+}
