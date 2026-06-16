e4546822fb1b3c8cf8d2d093724cbca652692a36
Abdul Reyza <abdulreyza6@gmail.com>
2026-06-16 15:46:13 +0700

    bikin order service

---
+++ lib/services/order_service.dart
@@ -0,0 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class OrderService {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<String> createOrder({
+    required List<Map<String, dynamic>> items,
+    required int totalAmount,
+  }) async {
+    final doc = await _firestore.collection('orders').add({
+      'userId': _auth.currentUser!.uid,
+      'items': items,
+      'totalAmount': totalAmount,
+      'status': 'pending',
+      'createdAt': Timestamp.now(),
+    });
+
+    return doc.id;
+  }
+}
